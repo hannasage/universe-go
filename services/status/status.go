101cbe87dd4cd4bc121a28de777480cd0545fa28
Kevin Haube <khaube@fearless.tech>
2022-11-04 21:42:20 -0400

    Moved checks to a goroutine

--- services/status/status.go
+++ services/status/status.go
@@ -1,7 +1,31 @@
 package status
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func StatusCheck(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Alive"))
+	services := []string{"thisService", "thatService", "theOtherService"}
+	var statuses []string
+
+	checkChannel := make(chan string)
+
+	go getFakeStatusesWorker(services, checkChannel)
+
+	for check := range checkChannel {
+		statuses = append(statuses, check)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(statuses)
+}
+
+func getFakeStatusesWorker(services []string, c chan string) {
+	for i, service := range services {
+		fmt.Printf("Checking: %s\n", service)
+		c <- fmt.Sprintf("%d-%s: alive", i, service)
+	}
+	close(c)
 }
