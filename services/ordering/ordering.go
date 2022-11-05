3e324b8860255aae16b97a645e84206f2fe39f1f
Kevin Haube <booking@kevinmhaube.com>
2022-11-05 17:23:32 -0400

    Update function, no export

--- services/ordering/ordering.go
+++ services/ordering/ordering.go
@@ -2,7 +2,7 @@ package ordering
 
 // Acts as the employee taking an order from a customer and building the
 // meal. Uses the IngredientLine to fulfill the order.
-func TakeOrder(orderId int) {
+func takeOrder(orderId int) {
 	// TODO: Interaction between employee and customer
 	println(orderId)
 }
@@ -10,6 +10,6 @@ func TakeOrder(orderId int) {
 // Goroutine for handling the customer <-> employee interaction
 func HandleCustomerQueue(c chan int) {
 	for customer := range c {
-		TakeOrder(customer)
+		takeOrder(customer)
 	}
 }
