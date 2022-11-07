ece8ced2960de4ce45af819407271fa90beb9bba
Kevin Haube <khaube@fearless.tech>
2022-11-06 19:08:39 -0500

    Start of fridge service

---
+++ services/fridge.go
@@ -0,0 +1,6 @@
+package services
+
+// Creates a channel and starts a Goroutine that adds new raw ingredients
+// to the kitchen fridge every n "weeks" of sim-time, and begins a goroutine
+// using the Prep handler to convert raw ingredients into prepared ingredients.
+func StartKitchenFridge() {}
